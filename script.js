ee8b506cdc14ef83eac1e4e4c30934b784e4583d
526LGreen@frhsd.com <526LGreen@frhsd.com>
2025-01-29 11:45:15 -0500

    Trying to put in my user choice and computer choice

--- script.js
+++ script.js
@@ -3,31 +3,24 @@ const userName = prompt('What is your name?');
 document.getElementById('userGreeting').innerText = `Hello! ${userName}, nice to meet you, I hope your excited to play!. Please pick from either Rock, Paper, or Scissors below!.`;
 
 
-
-
 const getUserChoice = (userInput) => {
     userInput = userInput.toLowerCase();
     if (
-      userInput === "rock" ||
-      userInput === "paper" ||
-      userInput === "scissors" ||
-      userInput === "bomb"
+      userInput === "Rock" ||
+      userInput === "Paper" ||
+      userInput === "Scissors" ||
     ) {
-      return userInput;
-      console.log(`User input : ${userInput}`);
+    return ( )
     } else {
       console.log("Not a valid input!");
     }
   };
-  
   const getComputerChoice = () => {
     return ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)];
   };
   
   const determineWinner = (userChoice, computerChoice) => {
-    if (userChoice === "bomb") {
-      return "You won!";
-    } else if (userChoice === "rock") {
+    } if (userChoice === "rock") {
       if (computerChoice === "paper") {
         return "The computer won!";
       } else {
@@ -48,10 +41,8 @@ const getUserChoice = (userInput) => {
     } else if (userChoice === computerChoice) {
       return "It's a tie!";
     }
-  };
   
   const playGame = () => {
-    let userChoice = getUserChoice("bomb");
     const computerChoice = getComputerChoice();
     console.log("You threw " + userChoice);
     console.log("The computer picked " + computerChoice);
