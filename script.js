6e9c59b67fe174be1ece2d8ae6146f107c567fe9
LGreen808 <526LGreen@frhsd.com>
2025-01-31 13:18:17 +0000

    Trying to put in my user choice on my game

--- script.js
+++ script.js
@@ -1,56 +1,40 @@
 //Asking the User Name and printing a welcome sentence on the console//
-const userName = prompt('What is your name?'); 
+const userName = prompt('What is your name?');
 document.getElementById('userName').innerText = userName
 document.getElementById('userGreeting').innerText = `Hello! ${userName}, nice to meet you, I hope your excited to play!. Please pick from either Rock, Paper, or Scissors below!.`;
 
 
-const getUserChoice = (userInput) => {
-    userInput = userInput.toLowerCase();
-    if (userInput === "Rock") 
-      userInput === "Paper" ||
-      userInput === "Scissors" ||
-    )
-    
-    {return ( )
+const getComputerChoice = () => {
+  return ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)];
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "The computer won!";
     } else {
-      console.log("Not a valid input!");
+      return "You won!";
     }
-  };
-  const getComputerChoice = () => {
-    return ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)];
-  };
-  
- 
- 
- 
-    const determineWinner = (userChoice, computerChoice) => {
-    } if (userChoice === "rock") {
-      if (computerChoice === "paper") {
-        return "The computer won!";
-      } else {
-        return "You won!";
-      }
-    } else if (userChoice === "paper") {
-      if (computerChoice === "rock") {
-        return "You won!";
-      } else {
-        return "The computer won!";
-      }
-    } else if (userChoice === "scissors") {
-      if (computerChoice === "rock") {
-        return "The computer won!";
-      } else {
-        return "You won!";
-      }
-    } else if (userChoice === computerChoice) {
-      return "It's a tie!";
+  } else if (userChoice === "paper") {
+    if (computerChoice === "rock") {
+      return "You won!";
+    } else {
+      return "The computer won!";
     }
-  
-  const playGame = () => {
-    const computerChoice = getComputerChoice();
-    console.log("You threw " + userChoice);
-    console.log("The computer picked " + computerChoice);
-    console.log(determineWinner(userChoice, computerChoice));
-  };
-  playGame();
-  
+  } else if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "The computer won!";
+    } else {
+      return "You won!";
+    }
+  } else if (userChoice === computerChoice) {
+    return "It's a tie!";
+  }
+}
+const playGame = (userChoice) => {
+  const computerChoice = getComputerChoice();
+  console.log("You threw " + userChoice);
+  console.log("The computer picked " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+};
+playGame();
